0c737364dde93f175cd4c71bd6a1c15869b86f84
Jordan Bogle <jordanbogle23@gmail.com>
2026-04-09 04:13:26 -0500

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import API from '../api/axiosConfig'; 
 import { User, Mail, Lock, HardHat, ArrowLeft, Loader2 } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
 
